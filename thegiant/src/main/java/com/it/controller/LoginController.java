8b719387d8f9bdf587608d6286d24bba0b3dda4f
thedestiny <xieyue86@163.com>
2016-07-04 17:50:21 +0800

    learn the spring mvc

---
+++ thegiant/src/main/java/com/it/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.it.controller;
+
+/**
+ * Created by xieyue on 2016/7/4.
+ */
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class LoginController {
+
+    Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "/login/{id:\\d+}")
+    public String login( @PathVariable Integer id){
+
+        logger.debug(" id is {}",id);
+        return "login";
+    }
+
+
+
+
+
+}
